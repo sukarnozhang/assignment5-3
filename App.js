0b2a2d2c25a6e74534a70f0d7b13daf99909d9df
sukarnozhang <sukarno.zhang@gmail.com>
2024-02-03 11:38:39 +0800

    first attempt

---
+++ App.js
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+}
+
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article!</Text>
+    </View>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const Drawer = createDrawerNavigator();
+
+export default function MyDrawer() {
+  return (
+    <>
+    <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Feed" component={Feed} />
+          <Drawer.Screen name="Article" component={Article} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+
+      <NavigationContainer>
+
+      <Tab.Navigator>
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+
+
+    </NavigationContainer>
+    </>
+  );
+}
+
+// export default function App() {
+//   console.log("hi there")
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+//   console.log("hi there")
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
